c14b7df1e63dd3e1d071ee8390d4855832fc1f13
Yegor <egorzhartun@gmail.com>
2020-11-24 09:35:32 +0300

    added new command to bot

--- acovid.js
+++ acovid.js
@@ -18,5 +18,42 @@ bot.start( ctx => ctx.reply(`
 
 bot.help( ctx => ctx.reply(HELP_BOT))
 
+bot.command('all', async (ctx) => {
+    try {
+        const allStats = await axios.get('https://disease.sh/v3/covid-19/all')
+        const {
+            updated,
+            cases,
+            todayCases,
+            deaths,
+            todayDeaths,
+            recovered,
+            todayRecovered,
+            active,
+            critical,
+            tests,
+            population,
+            affectedCountries
+        } = allStats.data
+        
+        const setData = `
+            Статистика обновлена около *${moment(updated).fromNow()}*.
+            Случаев: *${cases} чел.*
+            Случаев за сегодня: *${todayCases} чел.*
+            Смертей: *${deaths} чел.*
+            Смертей за сегодня: *${todayDeaths} чел.*
+            Выздоровевших: *${recovered} чел.*
+            Выздоровевших за сегодня: *${todayRecovered} чел.*
+            Активных случаев: *${active} чел.*
+            Критических случаев: *${critical} чел.*
+            Тесты: *${tests} чел.*
+            Население: *${population} чел.*
+            Пострадавшие страны: *${affectedCountries} чел.*
+        `
+        ctx.reply(setData, extra.markdown())
+    } catch(e) {
+        ctx.reply(e)
+    }
+})
 
 bot.launch()
